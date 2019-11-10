75896393d4e5e0b20960bd15d2b488b9e22239c0
nzhul <dobromirivanov1@gmail.com>
2019-11-10 18:05:22 +0200

    add events system, precompiled headers and Windows system

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,16 +1,28 @@
+#include "hzpch.h"
 #include "Application.h"
+
+#include "Hazel/Events/ApplicationEvent.h"
+#include "Hazel/Log.h"
+
+#include <GLFW/glfw3.h>
 
 namespace Hazel {
     Application::Application()
     {
+        _Window = std::unique_ptr<Window>(Window::Create());
     }
 
     Application::~Application()
     {
     }
 
-    void Application::Run() 
+    void Application::Run()
     {
-        while (true);
+        while (_Running)
+        {
+            glClearColor(1, 0, 1, 1);
+            glClear(GL_COLOR_BUFFER_BIT);
+            _Window->OnUpdate();
+        }
     }
 }
--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "Core.h"
+#include "Events/Event.h"
+#include <Hazel/Window.h>
 
 namespace Hazel
 {
@@ -11,6 +13,9 @@ namespace Hazel
         virtual ~Application();
 
         void Run();
+    private:
+        std::unique_ptr<Window> _Window;
+        bool _Running = true;
     };
 
     // To be defined in CLIENT
