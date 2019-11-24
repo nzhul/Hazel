95ca0ac564f5d249d18cf38d6eb5c4e1a7ce1044
nzhul <dobromirivanov1@gmail.com>
2019-11-24 20:26:06 +0200

    apply pull requests for various stuff

--- premake5.lua
+++ premake5.lua
@@ -17,14 +17,18 @@ IncludeDir["GLFW"] = "Hazel/vendor/GLFW/include"
 IncludeDir["Glad"] = "Hazel/vendor/Glad/include"
 IncludeDir["ImGui"] = "Hazel/vendor/imgui"
 
-include "Hazel/vendor/GLFW"
-include "Hazel/vendor/Glad"
-include "Hazel/vendor/imgui"
+group "Dependencies"
+    include "Hazel/vendor/GLFW"
+    include "Hazel/vendor/Glad"
+    include "Hazel/vendor/imgui"
+
+group ""
 
 project "Hazel"
     location "Hazel"
     kind "SharedLib"
     language "C++"
+    staticruntime "off"
 
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -57,7 +61,6 @@ project "Hazel"
 
     filter "system:windows"
         cppdialect "c++17"
-        staticruntime "On"
         systemversion "latest"
 
         defines
@@ -74,23 +77,24 @@ project "Hazel"
 
     filter "configurations:Debug"
         defines "HZ_DEBUG"
-        buildoptions "/MDd"
+        runtime "Debug"
         symbols "On"
 
     filter "configurations:Release"
         defines "HZ_RELEASE"
-        buildoptions "/MD"
+        runtime "Release"
         optimize "On"
 
     filter "configurations:Dist"
         defines "HZ_DIST"
-        buildoptions "/MD"
+        runtime "Release"
         optimize "On"
 
 project "Sandbox"
         location "Sandbox"
         kind "ConsoleApp"
         language "C++"
+        staticruntime "off"
     
         targetdir ("bin/" .. outputdir .. "/%{prj.name}")
         objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -114,7 +118,6 @@ project "Sandbox"
     
         filter "system:windows"
             cppdialect "c++17"
-            staticruntime "On"
             systemversion "latest"
     
             defines
@@ -124,15 +127,15 @@ project "Sandbox"
     
         filter "configurations:Debug"
             defines "HZ_DEBUG"
-            buildoptions "/MDd"
+            runtime "Debug"
             symbols "On"
     
         filter "configurations:Release"
             defines "HZ_RELEASE"
-            buildoptions "/MD"
+            runtime "Release"
             optimize "On"
     
         filter "configurations:Dist"
             defines "HZ_DIST"
-            buildoptions "/MD"
+            runtime "Release"
             optimize "On"
